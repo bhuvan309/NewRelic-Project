316fbf3f41dd9d7ab320e8f696c87b6aaf8e0a74
bhuvan309 <56302654+bhuvan309@users.noreply.github.com>
2019-10-10 20:18:30 +1100

    Files uploaded

---
+++ tic-tac-toe/nerdlets/my-awesome-nerdpack-nerdlet/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Game';
+
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class MyAwesomeNerdpack extends React.Component {
+    render() {
+        return (<div>
+           <Game></Game>
+        </div>);
+    }
+}
